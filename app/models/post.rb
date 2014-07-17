6cce56856001801d7c9f24a45c6d4716e3dda097
Hai Nguyen <haind.uet@gmail.com>
2014-07-17 09:23:51 +0700

    Post status, sharewith to text

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,22 @@ class Post < ActiveRecord::Base
   	has_many :comments
 
 	def status_text()
-		if status = -1
+		if status == -1
 			return 'Draft'
-		elsif status = 0
+		elsif status == 0
 			return 'Deactive'
 		else
 			return 'Active'
+		end
+	end
+
+	def sharewith_text()
+		if sharewith == 0
+			return 'Public'
+		elsif sharewith == 1
+			return 'Friend'
+		else
+			return 'Private'
 		end
 	end
 end
