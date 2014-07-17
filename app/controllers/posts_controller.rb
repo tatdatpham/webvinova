25dd58e9cd302698c5ec43adc91393fbb79e3f56
Hai Nguyen <haind.uet@gmail.com>
2014-07-17 09:54:54 +0700

    Show post of me

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.where user_id: session[:user_id]
   end
 
   # GET /posts/1
