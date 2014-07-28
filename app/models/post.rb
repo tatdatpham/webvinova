829c58ddee67272a9dd634102e38d21ae6ed730b
kunny171 <tatdat171@gmail.com>
2014-07-29 01:17:11 +0700

    fix css show post, edit  show time format on comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,38 @@ class Post < ActiveRecord::Base
 			return 'Active'
 		end
 	end
+	#get time on comment
+  	def get_time(created_at)
+	    difference = Time.now.to_i - created_at.to_i
+	    seconds    =  difference % 60
+	    difference = (difference - seconds) / 60
+	    minutes    =  difference % 60
+	    difference = (difference - minutes) / 60
+	    hours      =  difference % 24
+	    difference = (difference - hours) / 24
+	    days       =  difference % 7
+	    weeks      = (difference - days) / 7
+	    months     = days / 30
+	    years      = days / 365 
+	    if years > 0
+	    	return "#{years} year ago"
+	    elsif months > 0
+	    	return "#{months} month ago"
+	    elsif weeks > 0
+	    	return "#{weeks} week ago"
+	    elsif days > 0
+	    	return "#{days} day ago"
+	    elsif hours > 0
+	    	return "#{hours} hour ago"
+	    elsif minutes > 0
+	    	return "#{minutes} minute ago"
+	    elsif seconds > 3
+	    	return "#{seconds} second ago"
+	    else
+	    	return "Just now"
+	    end
+	end
+
 
 	def sharewith_text()
 		if sharewith == 0
