e20f966ece97747729fa989dc0d52f022d6a9e1d
kunny171 <tatdat171@gmail.com>
2014-08-04 09:31:18 +0700

    add fix show post on profile

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     #@user = User.find(params[:id])
     remain_connect_count()
     session[:current_tab] = 2
-    @posts = Post.all
+    @posts = Post.where status: '1'
     @last_posts = Post.limit(4).order('created_at DESC')
     @top_posts = Post.all.sort{ |a,b| b.votes.count <=> a.votes.count }.first(4)
     if params[:user_id] != nil
